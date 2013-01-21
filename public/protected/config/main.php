c6c7f6c18bd2752ce9df48e7a294735ff80c8843
weijie <walkskyer@88jianshen.com>
2013-01-21 14:41:42 +0800

    首页去版权

--- public/protected/config/main.php
+++ public/protected/config/main.php
@@ -6,7 +6,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'洗碗机设备厂家',
+	'name'=>'YiiCMS v1.0',
 	'language'=>'zh_cn',
 	// preloading 'log' component
 	'preload'=>array('log'),
