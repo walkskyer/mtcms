25575be52015f302c9ac7e173f564b61df79bdb0
weijie <walkskyer@88jianshen.com>
2013-01-21 10:19:25 +0800

    修正html引号。

--- public/protected/admin/views/layouts/column1.php
+++ public/protected/admin/views/layouts/column1.php
@@ -26,39 +26,39 @@ if($this->menupanel[0]==='content'):
 <div region="west" split="true" title="内容管理" iconCls="icon-cash" style="width:185px;padding1:1px;overflow:hidden;">
 	<div class="easyui-accordion" fit="true" border="false">
 
-		<div title="快捷菜单" <?php if($this->menupanel[1]==='short')echo "selected='true'";?>>
+		<div title="快捷菜单" <?php if($this->menupanel[1]==='short')echo 'selected="true"';?>>
 			<ul class="vlist">
 			<?php if(Yii::app()->params['menuoption']['content']['article']){ ?>
 				<li <?php if($this->menupanel[2]==='article_index')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('article/index',array('menupanel'=>'content|short|article_index'));?>">文章管理</a>
 				</li>
 			<?php }?>
-				
+
 			<?php if(Yii::app()->params['menuoption']['content']['product']){ ?>
 				<li <?php if($this->menupanel[2]==='product_index')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('product/index',array('menupanel'=>'content|short|product_index'));?>">产品管理</a>
 				</li>
 			<?php }?>
-				
-				
-			<?php if(Yii::app()->params['menuoption']['content']['link']){ ?>	
+
+
+			<?php if(Yii::app()->params['menuoption']['content']['link']){ ?>
 				<li <?php if($this->menupanel[2]==='link_index')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('link/index',array('menupanel'=>'content|short|link_index'));?>">链接管理</a>
 				</li>
-			<?php }?>	
-				
-			<?php if(Yii::app()->params['menuoption']['content']['picture']){ ?>		
+			<?php }?>
+
+			<?php if(Yii::app()->params['menuoption']['content']['picture']){ ?>
 				<li <?php if($this->menupanel[2]==='picture_index')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('picture/index',array('menupanel'=>'content|short|picture_index'));?>">图片广告管理</a>
 				</li>
 			<?php }?>
-			
+
 			<?php if(Yii::app()->params['menuoption']['content']['feedback']){ ?>
 				<li <?php if($this->menupanel[2]==='feedback_index')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('feedback/index',array('menupanel'=>'content|short|feedback_index'));?>">留言反馈管理</a>
 				</li>
 			<?php }?>
-				
+
 			<?php if(Yii::app()->params['menuoption']['content']['job']){ ?>
 				<li <?php if($this->menupanel[2]==='job_index')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('job/index',array('menupanel'=>'content|short|job_index'));?>">招聘职位管理</a>
@@ -66,10 +66,10 @@ if($this->menupanel[0]==='content'):
 			<?php }?>
 			</ul>
 		</div>
-	
-	
+
+
 	<?php if(Yii::app()->params['menuoption']['content']['article']){ ?>
-		<div title="文章"  <?php if($this->menupanel[1]==='article')echo "selected='true'";?>>
+		<div title="文章" <?php if($this->menupanel[1]==='article')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='article_create')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('article/create',array('menupanel'=>'content|article|article_create'));?>">添加文章</a>
@@ -83,9 +83,9 @@ if($this->menupanel[0]==='content'):
 			</ul>
 		</div>
 	<?php }?>
-	
+
 	<?php if(Yii::app()->params['menuoption']['content']['product']){ ?>
-		<div title="产品"  <?php if($this->menupanel[1]==='product')echo "selected='true'";?>>
+		<div title="产品" <?php if($this->menupanel[1]==='product')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='product_create')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('product/create',array('menupanel'=>'content|product|product_create'));?>">添加产品</a>
@@ -101,11 +101,11 @@ if($this->menupanel[0]==='content'):
 				</li>
 			</ul>
 		</div>
-	<?php }?>	
+	<?php }?>
 
-		
-	<?php if(Yii::app()->params['menuoption']['content']['link']){ ?>	
-		<div title="链接"  <?php if($this->menupanel[1]==='link')echo "selected='true'";?>>
+
+	<?php if(Yii::app()->params['menuoption']['content']['link']){ ?>
+		<div title="链接" <?php if($this->menupanel[1]==='link')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='link_create')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('link/create',array('menupanel'=>'content|link|link_create'));?>">添加链接</a>
@@ -118,10 +118,10 @@ if($this->menupanel[0]==='content'):
 				</li>
 			</ul>
 		</div>
-	<?php }?>	
-		
-	<?php if(Yii::app()->params['menuoption']['content']['picture']){ ?>		
-		<div title="图片广告"  <?php if($this->menupanel[1]==='picture')echo "selected='true'";?>>
+	<?php }?>
+
+	<?php if(Yii::app()->params['menuoption']['content']['picture']){ ?>
+		<div title="图片广告" <?php if($this->menupanel[1]==='picture')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='picture_create')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('picture/create',array('menupanel'=>'content|picture|picture_create'));?>">添加图片广告</a>
@@ -134,10 +134,10 @@ if($this->menupanel[0]==='content'):
 				</li>
 			</ul>
 		</div>
-	<?php }?>		
-	
+	<?php }?>
+
 	<?php if(Yii::app()->params['menuoption']['content']['feedback']){ ?>
-		<div title="留言反馈"  <?php if($this->menupanel[1]==='feedback')echo "selected='true'";?>>
+		<div title="留言反馈" <?php if($this->menupanel[1]==='feedback')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='feedback_create')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('feedback/create',array('menupanel'=>'content|feedback|feedback_create'));?>">添加留言反馈</a>
@@ -148,9 +148,9 @@ if($this->menupanel[0]==='content'):
 			</ul>
 		</div>
 	<?php }?>
-	
-	<?php if(Yii::app()->params['menuoption']['content']['job']){ ?>	
-		<div title="招聘职位"  <?php if($this->menupanel[1]==='job')echo "selected='true'";?>>
+
+	<?php if(Yii::app()->params['menuoption']['content']['job']){ ?>
+		<div title="招聘职位" <?php if($this->menupanel[1]==='job')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='job_create')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('job/create',array('menupanel'=>'content|job|job_create'));?>">添加招聘职位</a>
@@ -163,8 +163,8 @@ if($this->menupanel[0]==='content'):
 				</li>
 			</ul>
 		</div>
-	<?php }?>	
-		
+	<?php }?>
+
 	</div>
 </div>
 <?php
@@ -173,8 +173,8 @@ if($this->menupanel[0]==='usermanager'):
 ?>
 <div region="west" split="true" title="用户管理" iconCls="icon-cash" style="width:185px;padding1:1px;overflow:hidden;">
 	<div class="easyui-accordion" fit="true" border="false">
-	
-		<div title="用户管理" <?php if($this->menupanel[1]==='user')echo "selected='true'";?>>
+
+		<div title="用户管理" <?php if($this->menupanel[1]==='user')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='user_create')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('user/create',array('menupanel'=>'usermanager|user|user_create'));?>">添加用户</a>
@@ -192,7 +192,7 @@ if($this->menupanel[0]==='optionmanager'):
 ?>
 <div region="west" split="true" title="系统设置" iconCls="icon-cash" style="width:185px;padding1:1px;overflow:hidden;">
 	<div class="easyui-accordion" fit="true" border="false">
-		<div title="系统设置" <?php if($this->menupanel[1]==='system')echo "selected='true'";?>>
+		<div title="系统设置" <?php if($this->menupanel[1]==='system')echo 'selected="true"';?>>
 			<ul class="vlist">
 				<li <?php if($this->menupanel[2]==='site')echo "class='active'";?>>
 					<a href="<?php echo Yii::app()->createUrl('option/update',array('menupanel'=>'optionmanager|system|site'));?>">网站参数设置</a>
